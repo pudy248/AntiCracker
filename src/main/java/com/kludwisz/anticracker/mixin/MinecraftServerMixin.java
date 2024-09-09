892cf4bae2d2e67711708f9852843d539fee3a81
kludw <kludwisz@gmail.com>
2024-09-09 18:20:46 +0200

    Move injected method

--- src/main/java/com/kludwisz/anticracker/mixin/MinecraftServerMixin.java
+++ src/main/java/com/kludwisz/anticracker/mixin/MinecraftServerMixin.java
@@ -2,9 +2,7 @@ package com.kludwisz.anticracker.mixin;
 
 import com.kludwisz.anticracker.SeedHash;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.SaveProperties;
-import org.apache.commons.lang3.ObjectUtils;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MinecraftServerMixin {
 	@Shadow @Final protected SaveProperties saveProperties;
 
-	@Inject(at = @At("TAIL"), method = "loadWorld")
+	@Inject(at = @At("HEAD"), method = "createWorlds")
 	private void init(CallbackInfo info) {
 		// Pre-calculate the hash of the world seed on each new world load
 		long worldSeed = this.saveProperties.getGeneratorOptions().getSeed();
