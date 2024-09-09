6efcf9787712e4d7aadfc04ace1f623dc53bc299
kludw <kludwisz@gmail.com>
2024-09-09 18:14:35 +0200

    Fix NullPointerException

--- src/main/java/com/kludwisz/anticracker/mixin/MinecraftServerMixin.java
+++ src/main/java/com/kludwisz/anticracker/mixin/MinecraftServerMixin.java
@@ -2,17 +2,24 @@ package com.kludwisz.anticracker.mixin;
 
 import com.kludwisz.anticracker.SeedHash;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.SaveProperties;
+import org.apache.commons.lang3.ObjectUtils;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
-public class MinecraftServerMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
+public abstract class MinecraftServerMixin {
+	@Shadow @Final protected SaveProperties saveProperties;
+
+	@Inject(at = @At("TAIL"), method = "loadWorld")
 	private void init(CallbackInfo info) {
 		// Pre-calculate the hash of the world seed on each new world load
-		long worldSeed = ((MinecraftServer) (Object) this).getOverworld().getSeed();
+		long worldSeed = this.saveProperties.getGeneratorOptions().getSeed();
 		SeedHash.precalculateWorldSeedHash(worldSeed);
 	}
 }
